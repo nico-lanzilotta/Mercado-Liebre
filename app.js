7a58a1470540e96803e55711e3ca08109e7cfb1b
nico-lanzilotta <nicolanzilotta@hotmail.com>
2022-10-08 17:02:52 -0300

    creando el puerto de render y ajustando el packaje JSON

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ app.listen(3000, () => {
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const publicPath = path.resolve(__dirname, './public');
 
-app.listen(3000, () => console.log('Server on'));
+const PORT = process.env.PORT || 3000
+
+
+
+app.listen(PORT, () => console.log('Server on'));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'))
